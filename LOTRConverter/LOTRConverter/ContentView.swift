129fc9e6589bb925a96108f7e6c0a0ee88eca49b
Ricardo Dórame <rhdorame@gmail.com>
2024-02-27 09:51:23 -0600

    First commit for TLOR converter project

---
+++ LOTRConverter/LOTRConverter/ContentView.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  LOTRConverter
+//
+//  Created by Ricardo Dórame on 26/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    ZStack {
+      // Background image
+      VStack {
+        // Pony image
+        // Currency text
+        
+        // Currency Conversion section
+        HStack {
+          // Left part
+          VStack {
+            // Top part
+            HStack {
+              // Silver piece image
+              
+              // Silver piece text
+            }
+            // Input field
+          }
+          
+          // Center part
+            // Equals sign
+          // Right part
+          VStack {
+            // Top part
+            HStack {
+              // Gold piece text
+              
+              // Gold piece image
+            }
+            // Input field
+          }
+        }
+        
+        // Info button
+      }.padding()
+    }
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ LOTRConverter/LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Ricardo Dórame on 26/02/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
